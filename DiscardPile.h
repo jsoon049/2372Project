83888c41736071488d121142d801aaa924d2c08e
Amy Zhou <amyzhou@Amys-MacBook-Pro.local>
2020-11-17 15:47:45 -0500

    Setting up Project

---
+++ DiscardPile.h
@@ -0,0 +1,1 @@
+/*DiscardPile.h*/
---
+++ card.h
@@ -0,0 +1,24 @@
+/*Card.h*/
+#include <iostream>
+#include <cassert>      //for assert()
+using namespace std;
+/*
+enum bean { Blue, Chili, Stink, Green, soy, black, Red , garden };
+class Card {
+    public:
+        Card(bean b = Blue, int t = 1);
+        int value() { return val; }
+        void write();
+    private:
+        bean beans;
+        int val;
+        Chain chainValue;
+};
+ //Constructor 
+Card::Card(bean beans, int totalInDeck, Chain chainValue) {
+assert(t >= 1 && t <= 13);      //we use a standard function void assert (int expression) 
+                                //which indicates an error message if the expression is false.
+    bean = beans;
+    totalInDeck = t;
+}
+*/
