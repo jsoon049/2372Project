71a0e6497fdad1134d7b6c3358db61cf0abf24e4
Amy Zhou <amyzhou@Amys-MacBook-Pro.local>
2020-11-21 18:11:24 -0500

    Add friend function

--- card.h
+++ card.h
@@ -2,25 +2,19 @@
 #include <iostream>
 #include <cassert>      //for assert()
 using namespace std;
-//a card has beans, total in deck value 
-//and chain value(chain value has coin value in it)
 
 
 class Card {
 public:
 	virtual int getCardsPerCoin(int) = 0;
 	virtual string getName() = 0;
-
-//protected:
 	virtual void print(ostream&) const = 0;
-
-	/*friend ostream& operator << (ostream& o, const Card& c) {
-		c.print(o);
-		return o;
-	};
-	friend ostream& operator << (ostream& o, const Card* c) {
-		o << (*c);
-		return o;
-	};*/
+    
+	
+    //friend function that prints any objects
+    friend ostream& operator<<(std::ostream& o, const Card& c) {
+	    c.print(o);
+	    return o;
+    }
 };
 
