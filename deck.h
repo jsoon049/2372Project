6e4fdd97e8a4f4bf911c785ad6e13ff9c68fe0f5
Amy Zhou <amyzhou@Amys-MacBook-Pro.local>
2020-11-21 18:20:32 -0500

    setting up Deck class

--- deck.h
+++ deck.h
@@ -1,1 +1,13 @@
-/*Deck.h*/
+#define Deck_h
+
+#include "Card.h"
+#include <vector>
+#include "CardFactory.h"
+
+class Deck : public vector<Card*> {
+public:
+	Deck() = default;
+    //Deck(istream&, const CardFactory*)
+	//Card* draw();
+	friend ostream& operator<<(ostream&, const Deck&);
+};
